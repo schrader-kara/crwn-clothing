878a8211a296b5167d3bf5d44327197e9dc09305
Kara <schrader.kara@heb.com>
2021-11-23 18:03:16 -0600

    useRouter shop page
    Create shop page and display collection of items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
